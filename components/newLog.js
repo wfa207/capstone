b951ab2448dbfc4f5a31bd0f47b3b72f7a942d24
Tom Bradley <bradth01@gmail.com>
2016-06-30 18:30:16 -0400

    Log Navigator Functional

--- components/newLog.js
+++ components/newLog.js
@@ -7,11 +7,57 @@ import {
   Text,
   View,
   AsyncStorage,
+  Navigator,
   TouchableHighlight
 } from 'react-native';
 import styles from './styles';
 
 var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+//===========================================///
+
+// NAV HELPER FUNCTIONS
+
+///============================================///
+function genNavBarMapper(title) {
+  return {
+    LeftButton(route, navigator, index, navState) {
+      if (index > 0) {
+        return (
+          <TouchableHighlight
+          underlayColor="transparent"
+          onPress={() => navigator.pop()}>
+          <Text style={styles.leftNavButtonText}>Back</Text>
+          </TouchableHighlight>
+        );
+      } else {
+        return null;
+      }
+    },
+
+    RightButton(route, navigator, index, navState) {
+      return null;
+    },
+
+    Title(route, navigator, index, navState) {
+      return <Text style={styles.NavBarTitle}>{title}</Text>;
+    },
+  }
+}
+
+function genNavBar(title) {
+  return (
+    <Navigator.NavigationBar
+    style={styles.navBar}
+    routeMapper={genNavBarMapper(title)}/>
+  )
+}
+
+//===========================================///
+
+// Logs (activities/locations)
+
+///============================================///
 
 var Log = React.createClass({
 
@@ -25,6 +71,7 @@ var Log = React.createClass({
 
   componentDidMount() {
     this.fetchValueData(this.state.value);
+    console.log(this);
   },
 
   _onValueChange(value) {
@@ -48,6 +95,13 @@ var Log = React.createClass({
     });
   },
 
+  _navigate(rowData) {
+    rowData.type = this.state.value;
+    this.props.navigator.push({
+      passProps: rowData
+    });
+  },
+
   render() {
     return (
       <View style={styles.banner}>
@@ -61,7 +115,9 @@ var Log = React.createClass({
         <ListView
           dataSource={this.state.dataSource}
           renderRow={rowData=>
-            <TouchableHighlight style={styles.rowStyle}>
+            <TouchableHighlight
+            onPress={() => this._navigate(rowData)}
+            style={styles.rowStyle}>
               <Text>{rowData.name}</Text>
             </TouchableHighlight>}
         />
@@ -70,4 +126,72 @@ var Log = React.createClass({
   }
 });
 
-module.exports = Log;
+class LogDetailView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var isLocation = this.props.type === 'Locations';
+    if (isLocation) {
+      return (
+        <Text style={{marginTop: 120}}>
+          {this.props.name}
+          {this.props.city}
+          {this.props.state}
+          {this.props.country}
+          {this.props.timeSpent}
+        </Text>
+      )
+    } else {
+      return (
+        <Text style={{marginTop: 120}}>
+          {this.props.name}
+          {this.props.description}
+          {this.props.time_traveled}
+        </Text>
+      )
+    }
+  }
+}
+
+//===========================================///
+
+//LOG NAVIGATOR
+
+///============================================///
+
+class LogNav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderScene(route, navigator) {
+      if (route.name === 'Log Home') {
+        return <route.component
+        navigator={navigator}
+        {...route.passProps}
+        />
+      } else {
+        return <LogDetailView
+        navigator={navigator}
+        {...route.passProps}/>
+      }
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{
+          name: 'Log Home',
+          component: Log
+        }}
+        style={styles.banner}
+        renderScene={this.renderScene}
+        navigationBar={genNavBar('Log')}
+      />
+    );
+  }
+}
+
+module.exports = LogNav;
