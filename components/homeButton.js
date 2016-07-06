49435e0a1a9cac08761340091bbdaf82718bbb9a
Tom Bradley <bradth01@gmail.com>
2016-07-06 19:19:43 -0400

    more home button changes

--- components/homeButton.js
+++ components/homeButton.js
@@ -35,10 +35,6 @@ class HomeButton extends Component {
     console.log('STATE', this.state);
   }
 
-  onTyping (text) {
-    console.log(locations);
-  }
-
   getLocationName(locations, position, inputName) {
       // locations = JSON.parse(locations);
       let latitude = position.coords.latitude, longitude = position.coords.longitude;
@@ -63,69 +59,72 @@ class HomeButton extends Component {
         name: name,
         coordinates: [latitude, longitude]
       });
+      
       return AsyncStorage.setItem('locations', JSON.stringify(locations))
       .catch(console.error);
   }
 
-  getLocations() {
-    // var me = this;
-    this.setState({logging: !this.state.logging});
-    if (this.state.logging) this.setState({ inputShow: true });
-    return AsyncStorage.getItem('locations')
-    .then(locations => {
-      locations = JSON.parse(locations);
-      this.setState({
-        locations: locations
-      });
-      // function getLocationName(locations, inputName) {
-      //   locations = JSON.parse(locations);
-      //   let latitude = position.coords.latitude, longitude = position.coords.longitude;
-      //   let id = (locations ? locations.length : 0) + 1;
-      //   var name = inputName;
-      //
-      //   if (!name) {
-      //     let date = new Date(position.timestamp);
-      //     let hours = date.getHours();
-      //     let minutes = "0" + date.getMinutes();
-      //     let formattedTime = ((hours == 0) ? 12 : (hours % 12)) + ':' +
-      //     minutes.substr(-2) + (hours <= 12 ? "AM" : "PM");
-      //
-      //     let lat = (Math.abs(Math.floor(latitude*100)/100)).toString() + (latitude >= 0 ? "N" : "S");
-      //     let long = (Math.abs(Math.floor(longitude*100)/100)).toString() + (longitude >= 0 ? "E" : "W");
-      //
-      //     name = formattedTime + " | " + lat + ", " + long;
-      //   }
-      //
-      //   locations.push({
-      //     id: id,
-      //     name: name,
-      //     coordinates: [latitude, longitude]
-      //   });
-      //   return AsyncStorage.setItem('locations', JSON.stringify(locations))
-      //   .catch(console.error);
-      // }
-
-      // if (this.state.logging) {
-      //   AlertIOS.prompt('Location Name', 'Please enter a name for this location', [
-      //     {
-      //       text: 'Not Now',
-      //       onPress: () => getLocationName(),
-      //       style: 'destructive'
-      //     }, {
-      //       text: 'Enter',
-      //       onPress: text => getLocationName(text),
-      //       style: 'default'
-      //     }
-      //   ]);
-      // }
-    })
-    .catch(console.error);
-  }
+  // getLocations() {
+  //   // var me = this;
+  //   this.setState({logging: !this.state.logging});
+  //   if (this.state.logging) this.setState({ inputShow: true });
+  //   return AsyncStorage.getItem('locations')
+  //   .then(locations => {
+  //     locations = JSON.parse(locations);
+  //     this.setState({
+  //       locations: locations
+  //     });
+  //     // function getLocationName(locations, inputName) {
+  //     //   locations = JSON.parse(locations);
+  //     //   let latitude = position.coords.latitude, longitude = position.coords.longitude;
+  //     //   let id = (locations ? locations.length : 0) + 1;
+  //     //   var name = inputName;
+  //     //
+  //     //   if (!name) {
+  //     //     let date = new Date(position.timestamp);
+  //     //     let hours = date.getHours();
+  //     //     let minutes = "0" + date.getMinutes();
+  //     //     let formattedTime = ((hours == 0) ? 12 : (hours % 12)) + ':' +
+  //     //     minutes.substr(-2) + (hours <= 12 ? "AM" : "PM");
+  //     //
+  //     //     let lat = (Math.abs(Math.floor(latitude*100)/100)).toString() + (latitude >= 0 ? "N" : "S");
+  //     //     let long = (Math.abs(Math.floor(longitude*100)/100)).toString() + (longitude >= 0 ? "E" : "W");
+  //     //
+  //     //     name = formattedTime + " | " + lat + ", " + long;
+  //     //   }
+  //     //
+  //     //   locations.push({
+  //     //     id: id,
+  //     //     name: name,
+  //     //     coordinates: [latitude, longitude]
+  //     //   });
+  //     //   return AsyncStorage.setItem('locations', JSON.stringify(locations))
+  //     //   .catch(console.error);
+  //     // }
+  //
+  //     // if (this.state.logging) {
+  //     //   AlertIOS.prompt('Location Name', 'Please enter a name for this location', [
+  //     //     {
+  //     //       text: 'Not Now',
+  //     //       onPress: () => getLocationName(),
+  //     //       style: 'destructive'
+  //     //     }, {
+  //     //       text: 'Enter',
+  //     //       onPress: text => getLocationName(text),
+  //     //       style: 'default'
+  //     //     }
+  //     //   ]);
+  //     // }
+  //   })
+  //   .catch(console.error);
+  // }
 
   startStopLog() {
     getCurrentLocation(position => {
       this.setState({ position: position });
     });
+    this.setState({logging: !this.state.logging});
+    if (this.state.logging) this.setState({ inputShow: true });
     console.log('STATE2', this.state);
   }
 
